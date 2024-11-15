031d79dee88e37cfc941ba14a723be65bbe64aee
thuavanan2004 <thuavanan2004@gmail.com>
2024-11-15 14:42:05 +0700

    add roles, permissions, account

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,5 +1,5 @@
+import { useSelector } from "react-redux"; // Đảm bảo bạn đã import useSelector
 import "./style.scss";
-
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 import Dashboardicon from "../../assets/images/16139673.png";
@@ -12,106 +12,125 @@ import Tour from "../../assets/images/tour.png";
 import Transportations from "../../assets/images/transportations.png";
 
 function Sidebar() {
-    const title = "Trang chủ";
+  const title = "Trang chủ";
 
-    const navData = [
-        {
-            id: 1,
-            label: "Dashboard",
-            icon: <img src={Dashboardicon} alt="" className="icon" />,
-            to: "/dashboard",
-        },
-        {
-            id: 2,
-            label: "Tours",
-            icon: <img src={Tour} alt="" className="icon" />,
-            to: "/tour",
-        },
-        {
-            id: 3,
-            label: "Category",
-            icon: <img src={Category} alt="" className="icon" />,
-            to: "/categories",
-        },
-        {
-            id: 4,
-            label: "Depature",
-            icon: <img src={Depature} alt="" className="icon" />,
-            to: "/departures",
-        },
-        {
-            id: 5,
-            label: "Destination",
-            icon: <img src={Destination} alt="" className="icon" />,
-            to: "/destinations",
-        },
-        {
-            id: 6,
-            label: "Order",
-            icon: <img src={Order} alt="" className="icon" />,
-            to: "/orders",
-        },
-        {
-            id: 7,
-            label: "Transportations",
-            icon: <img src={Transportations} alt="" className="icon" />,
-            to: "/transportation",
-        },
-        {
-            id: 8,
-            label: "Roles",
-            icon: <img src={Role} alt="" className="icon" />,
-            to: "/roles",
-        },
-        {
-            id: 9,
-            label: "Permissions",
-            icon: <img src={Role} alt="" className="icon" />,
-            to: "/permissions",
-        },
-        {
-            id: 10,
-            label: "Account",
-            icon: <img src={Role} alt="" className="icon" />,
-            to: "/accounts",
-        },
-    ];
+  const permissions = useSelector((state) => state.permissions.list);
 
-    const getClassActive = ({ isActive }) => {
-        return isActive ? "active" : "";
-    };
+  const navData = [
+    {
+      id: 1,
+      label: "Dashboard",
+      icon: <img src={Dashboardicon} alt="" className="icon" />,
+      to: "/dashboard",
+      permission: "READ_DASHBOARD",
+    },
+    {
+      id: 2,
+      label: "Tours",
+      icon: <img src={Tour} alt="" className="icon" />,
+      to: "/tour",
+      permission: "READ_TOUR",
+    },
+    {
+      id: 3,
+      label: "Category",
+      icon: <img src={Category} alt="" className="icon" />,
+      to: "/categories",
+      permission: "READ_CATEGORY",
+    },
+    {
+      id: 4,
+      label: "Depature",
+      icon: <img src={Depature} alt="" className="icon" />,
+      to: "/departures",
+      permission: "READ_DEPARTURE",
+    },
+    {
+      id: 5,
+      label: "Destination",
+      icon: <img src={Destination} alt="" className="icon" />,
+      to: "/destinations",
+      permission: "READ_DESTINATION",
+    },
+    {
+      id: 6,
+      label: "Transportations",
+      icon: <img src={Transportations} alt="" className="icon" />,
+      to: "/transportations",
+      permission: "READ_TRANSPORTATION",
+    },
+    {
+      id: 7,
+      label: "Order",
+      icon: <img src={Order} alt="" className="icon" />,
+      to: "/orders",
+      permission: "READ_ORDER",
+    },
+    {
+      id: 8,
+      label: "Roles",
+      icon: <img src={Role} alt="" className="icon" />,
+      to: "/roles",
+      permission: "READ_ROLE",
+    },
+    {
+      id: 9,
+      label: "Permissions",
+      icon: <img src={Role} alt="" className="icon" />,
+      to: "/permissions",
+      permission: "READ_PERMISSIONS",
+    },
+    {
+      id: 10,
+      label: "Account",
+      icon: <img src={Role} alt="" className="icon" />,
+      to: "/accounts",
+      permission: "READ_ACCOUNT",
+    },
+  ];
 
-    return (
-        <div className="sidebar">
-            <NavLink to="/" className="sidebar__header flex-aline-center">
-                <img
-                    src={Logo}
-                    alt="logo hit with gradient border"
-                    className="sidebar__header--logo"
-                />
-                <strong className="sidebar__header--title">{title}</strong>
-            </NavLink>
+  const hasPermission = (permission) => {
+    return permissions.includes(permission);
+  };
 
-            <nav className="sidebar__nav">
-                <ul>
-                    {navData.map((navItem) => (
-                        <li className="sidebar__nav--item" key={navItem.id}>
-                            <NavLink
-                                key={navItem.id}
-                                to={navItem.to}
-                                className={({ isActive }) =>
-                                    `flex-aline-center ${getClassActive({ isActive })}`
-                                }
-                            >
-                                {navItem.icon}
-                                <strong>{navItem.label}</strong>
-                            </NavLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </div>
-    );
+  const getClassActive = ({ isActive }) => {
+    return isActive ? "active" : "";
+  };
+
+  return (
+    <div className="sidebar">
+      <NavLink to="/" className="sidebar__header flex-aline-center">
+        <img
+          src={Logo}
+          alt="logo hit with gradient border"
+          className="sidebar__header--logo"
+        />
+        <strong className="sidebar__header--title">{title}</strong>
+      </NavLink>
+
+      <nav className="sidebar__nav">
+        <ul>
+          {navData.map(
+            (navItem) =>
+              hasPermission(navItem.permission) && (
+                <li className="sidebar__nav--item" key={navItem.id}>
+                  <NavLink
+                    key={navItem.id}
+                    to={navItem.to}
+                    className={({ isActive }) =>
+                      `flex-aline-center ${getClassActive({ isActive })}`
+                    }
+                  >
+                    {navItem.icon}
+                    <strong>{navItem.label}</strong>
+                  </NavLink>
+                </li>
+              )
+          )}
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,62 +1,59 @@
-import React from 'react';
-import './style.scss';
-import { Button, Checkbox, Form, Input, message } from 'antd';
-import { login } from '../../api/auth';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./style.scss";
+import { Button, Checkbox, Form, Input, message } from "antd";
+import { login } from "../../api/auth";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const navigate = useNavigate();
-    const onFinish = async (values) => {
-        try {
-            await login(values);
-            message.success('Đăng nhập thành công!');
-            navigate('/dashboard')
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      await login(values);
+      navigate("/dashboard");
+      message.success("Đăng nhập thành công!");
+    } catch (error) {
+      console.error(error);
+      message.error("Đăng nhập thất bại!");
+    }
+  };
+  return (
+    <>
+      <div className="login-container">
+        <div className="login-content">
+          <Form className="form-login" onFinish={onFinish}>
+            <h2>LOGIN</h2>
+            <Form.Item
+              className="form-input"
+              label="email"
+              name="email"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-        } catch (error) {
-            console.error(error);
-            message.error('Đăng nhập thất bại!');
-        }
-    };
-    return (
-        <>
-            <div className="login-container">
-                <div className="login-content">
+            <Form.Item
+              className="form-input"
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-                    <Form
-                        className='form-login'
-                        onFinish={onFinish}
-                    >
-                        <h2>LOGIN</h2>
-                        <Form.Item
-                            className='form-input'
-                            label="email"
-                            name="email"
-                            rules={[{ required: true, message: 'Please input your username!' }]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-                        <Form.Item
-                            className='form-input'
-                            label="Password"
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your password!' }]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
-
-                        <Form.Item >
-                            <Button className='button' type="primary" htmlType="submit">
-                                Login
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
-
-            </div>
-        </>
-
-    )
+            <Form.Item>
+              <Button className="button" type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
